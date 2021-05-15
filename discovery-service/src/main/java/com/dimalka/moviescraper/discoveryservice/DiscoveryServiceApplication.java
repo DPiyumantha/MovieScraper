6a2774220d224be0b5127f89e0cb9ea21d67374e
dpiyumantha <dimalkapiyumantha@gmail.com>
2021-05-15 16:02:19 +0530

    dockerizing

--- discovery-service/src/main/java/com/dimalka/moviescraper/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/dimalka/moviescraper/discoveryservice/DiscoveryServiceApplication.java
@@ -1,8 +1,14 @@
 package com.dimalka.moviescraper.discoveryservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +18,25 @@ public class DiscoveryServiceApplication {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
+}
+@RestController
+class EurekaClientRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/hello")
+    public @ResponseBody
+    String sayHello() {
+        return "Hello!";
+    }
+
+    @RequestMapping("/clients/{applicationName}")
+    public @ResponseBody
+    String getClientsByApplicationName(@PathVariable String applicationName) {
+        if(this.discoveryClient.getInstances(applicationName).size()==0){
+            return "No instances";
+        }
+        return this.discoveryClient.getInstances(applicationName).get(0).getUri().toString();
+    }
 }
--- scraping-service/src/main/java/com/dimalka/moviescraper/scrapingservice/controller/MainController.java
+++ scraping-service/src/main/java/com/dimalka/moviescraper/scrapingservice/controller/MainController.java
@@ -41,5 +41,10 @@ public class MainController {
         return new ResponseEntity(movies, HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
 
 }
